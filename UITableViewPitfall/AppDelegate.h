03e1100657c9b41f1743df2ce063c6e1d2f6cc91
Jonathan Dang <jonakyd@gmail.com>
2016-03-01 11:00:19 +0800

    Initial Commit

---
+++ UITableViewPitfall/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewPitfall
+//
+//  Created by Jonathan Dang on 2016/3/1.
+//  Copyright © 2016年 Kingyao Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewPitfall/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewPitfall
+//
+//  Created by Jonathan Dang on 2016/3/1.
+//  Copyright © 2016年 Kingyao Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
